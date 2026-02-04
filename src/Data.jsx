888cc30747b85de55b7fc7b5379b088f4bea2d89
Mandarisse <moahmedmandarisse@gmail.com>
2026-02-04 22:12:50 +0100

    feat: add testimonials data (#10)

--- src/Data.jsx
+++ src/Data.jsx
@@ -91,3 +91,40 @@ export const teamMembers = [
   },
 ];
 
+export const testimonials = [
+  {
+    id: 1,
+    review:
+      '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    author: 'John Smith',
+    position: 'Marketing Director at XYZ Corp',
+  },
+  {
+    id: 2,
+    review:
+      '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    author: 'John Smith',
+    position: 'Marketing Director at XYZ Corp',
+  },
+  {
+    id: 3,
+    review:
+      '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    author: 'John Smith',
+    position: 'Marketing Director at XYZ Corp',
+  },
+  {
+    id: 4,
+    review:
+      '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    author: 'John Smith',
+    position: 'Marketing Director at XYZ Corp',
+  },
+  {
+    id: 5,
+    review:
+      '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    author: 'John Smith',
+    position: 'Marketing Director at XYZ Corp',
+  },
+];

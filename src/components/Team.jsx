e4a39e976022557734ff4a22fa4a6d14aecf0562
Mandarisse <moahmedmandarisse@gmail.com>
2026-02-04 23:45:56 +0100

    feat: render team members from data file (#12)

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import SectionHeading from './ui/SectionHeading';
-import TeamCard from './ui/TeamCard';
-import { teamMembers } from '../Data';
-import Button from './ui/Button';
+import React from "react";
+import SectionHeading from "./ui/SectionHeading";
+import TeamCard from "./ui/TeamCard";
+import { teamMembers } from "../Data";
+import Button from "./ui/Button";
 
 const Team = () => {
   return (
-    <section className="mx-auto container px-6 py-12">
+    <section id="team" className="mx-auto container px-6 py-12">
       <div className="heading">
         <SectionHeading
           text="Meet the skilled and experienced team behind our successful digital marketing strategies"
@@ -16,19 +16,19 @@ const Team = () => {
 
       <div className="cards">
         <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {teamMembers.map((member) => {
-            return (
-              <div className="border border-black p-8 rounded-3xl shadow-[0_4px_0_0_#000]">
-                <TeamCard
-                  picture={member.picture}
-                  name={member.name}
-                  position={member.position}
-                  bio={member.bio}
-                  key={member.id}
-                />
-              </div>
-            );
-          })}
+          {teamMembers.map((member) => (
+            <div
+              key={member.id}
+              className="border border-black p-8 rounded-3xl shadow-[0_4px_0_0_#000]"
+            >
+              <TeamCard
+                picture={member.picture}
+                name={member.name}
+                position={member.position}
+                bio={member.bio}
+              />
+            </div>
+          ))}
         </div>
 
         <div className="flex justify-center md:justify-end items-center px-8">
